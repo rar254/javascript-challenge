ba6799f228c8a22bf068a355deeb36da54fbad79
Rafael <stephany@Rafaels-Air.fios-router.home>
2021-02-03 17:53:25 -0500

    added alien table to htlm file

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log(tableData);
+
+//Select our tbody to add our table
+var tbody = d3.select("tbody");
+
+//loop through our data to insert proper amount of rows for our alien data
+tableData.forEach(function(aliens){
+    var row = tbody.append("tr");
+
+    // Use object.entries to loop through tableData array [key, value]
+    Object.entries(aliens).forEach(function([key,value]){
+
+        // append ("rd") for each key and place their value(text)
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
